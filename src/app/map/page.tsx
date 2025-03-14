9366f5f0a74a5c2abe345e0029f19304a078d0b0
severedseikyo <thanmaysadguru5912@gmail.com>
2025-03-14 19:01:04 +0530

    vansh update/map page

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import {ScreenOrientation} from "@capacitor/screen-orientation";
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 import MapItem from "@/components/MapItem"
@@ -10,12 +11,29 @@ import { FaToggleOff, FaToggleOn } from "react-icons/fa6";
 export default function Map() {
     const [isSatView, setIsSatView] = useState(false);
     const [isHelpDes, setIsHelpDes] = useState(false);
+    const [isPortrait, setIsPortrait]=useState(true)
 
     // Function to toggle view
     const toggleView = () => {
         setIsSatView(prev => !prev);
         setIsHelpDes(false); // Reset help desk info when toggling the view
     };
+
+    //Function to check Screen Orientation
+    const checkOrientation=async()=>{
+        const orientation=await ScreenOrientation.orientation();
+        if (orientation.type.includes('landscape')){
+            setIsPortrait(false)
+        }else{
+            setIsPortrait(true)
+        }
+    }
+
+    //Listen for Screen Orientation
+    ScreenOrientation.addListener("screenOrientationChange",(event)=>{
+        console.log(event.type)
+        checkOrientation();
+    })
 
     return (
         <div className="flex flex-col min-h-screen">
@@ -27,7 +45,7 @@ export default function Map() {
                         className="absolute top-4 right-4 p-2 text-white transition-all duration-200">
                         {isSatView ? <FaToggleOn fontSize={30}/> : <FaToggleOff fontSize={30}/>}
                     </button>
-                    <MapItem isSatView={isSatView} isHelpDes={isHelpDes} setIsHelpDes={setIsHelpDes}/>
+                    <MapItem isSatView={isSatView} isHelpDes={isHelpDes} setIsHelpDes={setIsHelpDes} isPortrait={isPortrait}/>
                 </div>
             </main>
             <Footer />
