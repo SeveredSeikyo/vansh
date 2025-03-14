d753252d46b1b97c9ca6555b46d54877e93f5dc5
severedseikyo <thanmaysadguru5912@gmail.com>
2025-03-14 14:37:52 +0530

    vansh update/stalls page

--- src/app/stalls/page.tsx
+++ src/app/stalls/page.tsx
@@ -1,7 +1,14 @@
+import Footer from "@/components/Footer"
+import Header from "@/components/Header"
+
 export default function stalls(){
     return(
         <div>
-            <h1>Hii</h1>
+            <Header/>
+            <main className="flex-1 pt-16 pb-16">
+                <h1>Hii</h1>
+            </main>
+            <Footer/>
         </div>
     )
 }
