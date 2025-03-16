c8a90b3ac8d72a0019d0d7f5316da9cbf9f95f3b
severedseikyo <thanmaysadguru5912@gmail.com>
2025-03-17 01:19:45 +0530

    vansh update/map section updated using leaflet

--- src/app/stalls/page.tsx
+++ src/app/stalls/page.tsx
@@ -6,7 +6,7 @@ export default function stalls(){
         <div>
             <Header/>
             <main className="flex-1 pt-16 pb-16">
-                <h1>Hii</h1>
+                <div></div>
             </main>
             <Footer/>
         </div>
